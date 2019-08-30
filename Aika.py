d03453a85688e31009f44f441a9f096ce298aa16
cmyui <cmyuiosu@gmail.com>
2019-08-30 06:48:46 -0400

    redundant

--- Aika.py
+++ Aika.py
@@ -299,8 +299,7 @@ async def on_message(message):
         await message.delete()
 
         if not any(required in message.content for required in ("akatsuki.pw", "osu.ppy.sh")) or len(message.content) > 58: # Should not EVER be over 58 characters. (57 but safe)
-            await message.author.send("Your beatmap request was incorrectly formatted, and thus has not been submitted. "
-                                      "Please use the old osu links for the time being. (e.g. https://osu.ppy.sh/b/123)")
+            await message.author.send("Your beatmap request was incorrectly formatted, and thus has not been submitted.")
             return
 
         # Support both links like "https://osu.ppy.sh/b/123" AND "osu.ppy.sh/b/123".
